46312eca64bc467d4497c0143d1c22da955b2109
julia-prestes <contatojuliaprestes@gmail.com>
2022-02-01 14:19:41 -0300

    prj-final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
-import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
+import { FormsModule } from '@angular/forms';
+import { ExtratoComponent } from './extrato/extrato.component';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+import { AppRoutingModule } from './app.routing.module';
+
+registerLocaleData(localePt, 'pt');
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NovaTransferenciaComponent
+  declarations: [AppComponent, NovaTransferenciaComponent, ExtratoComponent],
+  imports: [BrowserModule, FormsModule, HttpClientModule, AppRoutingModule],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'pt' },
+    {
+      provide: DEFAULT_CURRENCY_CODE,
+      useValue: 'BRL',
+    },
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,11 +1,40 @@
-import { Component } from "@angular/core";
+import { Transferencia } from './../services/models/transferencias.model';
+import { Component, Output } from '@angular/core';
+import { EventEmitter } from '@angular/core';
+import { TransferenciaService } from '../services/transferencia.service';
+import { Router } from '@angular/router';
 
 @Component({
-    selector: 'app-nova-transferencia',
-    templateUrl: './nova-transferencia.component.html' ,
-    styleUrls:['./nova-transferencia.component.scss']
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss'],
 })
+export class NovaTransferenciaComponent {
+  @Output() aoTransferir = new EventEmitter<any>();
 
-export class NovaTransferenciaComponent{
+  valor: number;
+  destino: number;
 
+  constructor(private service: TransferenciaService, private router: Router) {}
+
+  transferir() {
+    console.log('Solicitada nova transferência');
+    const valorEmitir: Transferencia = {
+      valor: this.valor,
+      destino: this.destino,
+    };
+    this.service.adicionar(valorEmitir).subscribe(
+      (resultado) => {
+        console.log(resultado);
+        this.limparCampos();
+        this.router.navigateByUrl('extrato');
+      },
+      (error) => console.log(error)
+    );
+  }
+
+  limparCampos() {
+    this.valor = 0;
+    this.destino = 0;
+  }
 }
